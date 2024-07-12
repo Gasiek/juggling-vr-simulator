31a2593d3f451c7f7f8c8510567b4628222d4f04
gasiek <kacper.b.gasior@gmail.com>
2024-07-12 23:33:38 +0200

    cleaned up the script and removed magic numbers

--- Assets/Scripts/TutorialAnimationController.cs
+++ Assets/Scripts/TutorialAnimationController.cs
@@ -8,24 +8,33 @@ public class TutorialAnimationController : MonoBehaviour
     public Transform throwingPointRight;
     public Transform catchingPointLeft;
     public Transform catchingPointRight;
-    public Rigidbody[] ballsRigidbodies;
+    public List<Rigidbody> ballsRigidbodies;
     public float timeOfTransitionToThrowingPosition;
     public float upwardForce; // 3.5
     public float insideForce; // 0.43
+    public float gravityCounter;
+    public float delayAfterThrowingAllBalls;
+    private List<Rigidbody> currentBallsRigidbodies = new();
     private int currentBallIndex = 0;
+    private int ballsToThrow;
     private bool didPreviousBallReachPeak = true;
-    public float gravityCounter = 5;
+    private bool shouldLoop = false;
 
     void Start()
     {
         foreach (Rigidbody ballRb in ballsRigidbodies)
         {
             ballRb.useGravity = false;
+            ballRb.gameObject.SetActive(false);
         }
+
         ballsRigidbodies[0].transform.position = catchingPointRight.position;
         ballsRigidbodies[1].transform.position = catchingPointLeft.position;
         ballsRigidbodies[2].transform.position = catchingPointRight.position;
-        ShowThreeBallsTutorial();
+
+        ShowOneBallTutorial();
+        // ShowTwoBallsTutorial();
+        // ShowThreeBallsTutorial(newShouldLoop: false);
     }
 
     void FixedUpdate()
@@ -35,7 +44,7 @@ public class TutorialAnimationController : MonoBehaviour
 
     private void ApplyCustomGravity()
     {
-        foreach (Rigidbody ballRb in ballsRigidbodies)
+        foreach (Rigidbody ballRb in currentBallsRigidbodies)
         {
             if (ballRb.useGravity)
             {
@@ -44,30 +53,92 @@ public class TutorialAnimationController : MonoBehaviour
         }
     }
 
-    public void ShowThreeBallsTutorial()
+    public void ShowThreeBallsTutorial(bool newShouldLoop = false)
     {
+        shouldLoop = newShouldLoop;
+        InitializeTutorial(3);
         StartCoroutine(JugglingLoop());
     }
 
-    private void ThrowBallFromRightHand(Rigidbody ballRb)
+    public void ShowTwoBallsTutorial()
     {
-        ballRb.useGravity = true;
-        ballRb.velocity = Vector3.zero;
-        ballRb.AddForce(Vector3.up * upwardForce + Vector3.left * insideForce, ForceMode.Impulse);
+        InitializeTutorial(2);
+        StartCoroutine(JugglingLoop());
     }
 
-    private void ThrowBallFromLeftHand(Rigidbody ballRb)
+    public void ShowOneBallTutorial()
+    {
+        InitializeTutorial(1);
+        StartCoroutine(JugglingLoop());
+    }
+
+    private void InitializeTutorial(int ballCount)
+    {
+        currentBallsRigidbodies = new List<Rigidbody>(ballsRigidbodies.GetRange(0, ballCount));
+        foreach (Rigidbody ballRb in currentBallsRigidbodies)
+        {
+            ballRb.gameObject.SetActive(true);
+        }
+        ballsToThrow = ballCount;
+    }
+
+    private IEnumerator JugglingLoop()
+    {
+        yield return new WaitForSeconds(1);
+        while (true)
+        {
+            if (didPreviousBallReachPeak)
+            {
+                if (ballsToThrow == 0)
+                {
+                    yield return new WaitForSeconds(delayAfterThrowingAllBalls);
+                    ballsToThrow = currentBallsRigidbodies.Count;
+                }
+                Rigidbody currentBallRb = currentBallsRigidbodies[currentBallIndex];
+                StartCoroutine(SmoothlyMoveBallToThrowingPositionAndThrow(currentBallRb));
+                didPreviousBallReachPeak = false;
+                if (!shouldLoop)
+                {
+                    ballsToThrow--;
+                }
+                currentBallIndex = (currentBallIndex + 1) % currentBallsRigidbodies.Count;
+                StartCoroutine(WaitForBallToReachPeak(currentBallRb));
+                StartCoroutine(WaitForBallToReachHandHeight(currentBallRb));
+            }
+            yield return null;
+        }
+    }
+
+    private void ThrowBall(Rigidbody ballRb)
     {
         ballRb.useGravity = true;
         ballRb.velocity = Vector3.zero;
-        ballRb.AddForce(Vector3.up * upwardForce + Vector3.right * insideForce, ForceMode.Impulse);
+        Vector3 force;
+        if (ballRb.transform.position.x > 0)
+        {
+            force = Vector3.up * upwardForce + Vector3.left * insideForce;
+        }
+        else
+        {
+            force = Vector3.up * upwardForce + Vector3.right * insideForce;
+        }
+
+        ballRb.AddForce(force, ForceMode.Impulse);
     }
 
     private IEnumerator SmoothlyMoveBallToThrowingPositionAndThrow(Rigidbody ballRb)
     {
         float elapsedTime = 0;
         Vector3 startingPosition = ballRb.transform.position;
-        Vector3 targetPosition = ballRb.transform.position.x > 0 ? throwingPointRight.position : throwingPointLeft.position;
+        Vector3 targetPosition;
+        if (ballRb.transform.position.x > 0)
+        {
+            targetPosition = throwingPointRight.position;
+        }
+        else
+        {
+            targetPosition = throwingPointLeft.position;
+        }
 
         while (elapsedTime < timeOfTransitionToThrowingPosition)
         {
@@ -78,46 +149,26 @@ public class TutorialAnimationController : MonoBehaviour
         }
 
         ballRb.transform.position = targetPosition;
-
-        if (ballRb.transform.position.x > 0)
-        {
-            ThrowBallFromRightHand(ballRb);
-        }
-        else
-        {
-            ThrowBallFromLeftHand(ballRb);
-        }
+        ThrowBall(ballRb);
     }
 
-    private IEnumerator JugglingLoop()
-    {
-        yield return new WaitForSeconds(1);
-        while (true)
-        {
-            if (didPreviousBallReachPeak)
-            {
-                Rigidbody currentBallRb = ballsRigidbodies[currentBallIndex];
-                StartCoroutine(SmoothlyMoveBallToThrowingPositionAndThrow(currentBallRb));
-                didPreviousBallReachPeak = false;
-                currentBallIndex = (currentBallIndex + 1) % ballsRigidbodies.Length;
-                StartCoroutine(WaitForBallToReachPeak(currentBallRb));
-                StartCoroutine(WaitForBallToReachHandHeight(currentBallRb));
-            }
-            yield return null;
-        }
-    }
     private IEnumerator WaitForBallToReachHandHeight(Rigidbody ballRb)
     {
         while (ballRb.transform.position.y >= throwingPointLeft.position.y)
         {
             yield return null;
         }
-        Debug.Log("ball y: " + ballRb.transform.position.y);
-        Debug.Log("throwing point y: " + throwingPointLeft.position.y);
         ballRb.useGravity = false;
         ballRb.velocity = Vector3.zero;
         ballRb.angularVelocity = Vector3.zero;
-        ballRb.transform.position = ballRb.transform.localPosition.x > 0 ? catchingPointRight.position : catchingPointLeft.position;
+        if (ballRb.transform.localPosition.x > 0)
+        {
+            ballRb.transform.position = catchingPointRight.position;
+        }
+        else
+        {
+            ballRb.transform.position = catchingPointLeft.position;
+        }
     }
 
     private IEnumerator WaitForBallToReachPeak(Rigidbody ballRb)
